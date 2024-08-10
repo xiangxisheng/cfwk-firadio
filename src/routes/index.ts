cc5ff6f7c3a6f0cde565d743548d48b646548278
x <x@x.x>
2024-08-10 17:02:32 +0800

    统计访问次数，启动时间，访问的路径、用户IP等等 这个代码可以让我直观的分析worker的缓存功能

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,6 +10,37 @@ app.onError((err, c) => {
 	return c.json({ message: msg }, 500);
 });
 
+const status = {
+	reqcount: 0,
+	reqips: {} as Record<string, number>,
+	reqpaths: {} as Record<string, number>,
+	started: 0,
+	uptimems: 0,
+};
+
+app.use('*', async (c: any, next) => {
+	if (status.started === 0) {
+		// 在这里处理状态的初始化
+		status.started = Date.now();
+	}
+	status.uptimems = Date.now() - status.started;
+	status.reqcount++;
+	const clientIP = c.req.header("CF-Connecting-IP");
+	if (!status.reqips[clientIP]) {
+		status.reqips[clientIP] = 0;
+	}
+	status.reqips[clientIP]++;
+	if (!status.reqpaths[c.req.path]) {
+		status.reqpaths[c.req.path] = 0;
+	}
+	status.reqpaths[c.req.path]++;
+	return await next();
+});
+
+app.get("/api/status", async (c) => {
+	return c.json({ status });
+});
+
 app.route('/api', require('./api').default);
 
 app.route('/', require('@/utils/route/vben').default);
