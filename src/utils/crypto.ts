124a0dd9c3325e9a3b8f9790dc50a71ae06c3fe0
x <x@x.x>
2024-08-21 00:04:04 +0800

    修复在本地环境下无法使用md5加密

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,13 +1,26 @@
+const modules: Record<string, any> = {};
 
-export async function md5(content: string): Promise<string> {
+async function cf_crypto_encode(name: string, content: string): Promise<string> {
 	const myDigest = await crypto.subtle.digest(
 		{
-			name: 'MD5',
+			name,
 		},
 		new TextEncoder().encode(content)
 	);
-	return [...new Uint8Array(myDigest)]
-		.map(b => b.toString(16).padStart(2, '0'))
-		.join('')
+	return [...new Uint8Array(myDigest)].map((b) => b.toString(16).padStart(2, '0')).join('');
 }
 
+function node_crypto_encode(name: string, content: string) {
+	const moduleName = 'crypto';
+	modules.crypto = modules.crypto ? modules.crypto : require(moduleName);
+	const hash = modules.crypto.createHash(name);
+	hash.update(content);
+	return hash.digest('hex');
+}
+
+export async function md5(content: string): Promise<string> {
+	try {
+		return await cf_crypto_encode('MD5', content);
+	} catch (e) {}
+	return node_crypto_encode('MD5', content);
+}
